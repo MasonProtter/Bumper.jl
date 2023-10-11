bb48d79c568ef5b41f0b29822420fa68a2fbdd6b
Mason Protter <mason.protter@icloud.com>
2023-10-11 18:03:53 +0200

    reject all uses of `@goto` and `return` inside `@no_escape`

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,12 +18,6 @@ function f(x, buf::AllocBuffer)
     end
 end
 
-function returns_early()
-    @no_escape begin
-        return sum(alloc(Int, 10) .= 1)
-    end
-end
-
 @testset "basic" begin
     v = [1,2,3]
     b = AllocBuffer(100)
@@ -35,9 +29,51 @@ end
     @test @allocated(f(v, b)) == 0
 
     @test b.offset == 0
+    
+    @test_throws Exception alloc(Int, b, 100000)
+    Bumper.reset_buffer!(b)
+    @test_throws Exception @no_escape begin
+        alloc(Int, 10)
+    end
+end
 
-    @test returns_early() == 10
-    @test default_buffer().offset == 0
+macro sneaky_return(ex)
+    esc(:(return $ex))
+end
+
+macro sneaky_goto(label)
+    esc(:(@goto $label))
+end
+
+@testset "trying to break out of no_escape blocks" begin
+    # It is very tricky to properly deal with code which uses @goto or return inside
+    # a @no_escape code block, because could bypass the mechanism for resetting the
+    # buffer's offset after the block completes.
+    
+    # I played with some mechanisms for cleaning it up, but they were sometimes incorrect
+    # if one nested multuple @no_escape blocks, so I decided that they should simply be
+    # disabled, and throw an error at macroexpansion time.
+    
+    @test_throws Exception Bumper.Internals._no_escape_macro(
+        :(default_buffer()),
+        :(return sum(alloc(Int, 10) .= 1)),
+        @__MODULE__()
+    )
+    @test_throws Exception Bumper.Internals._no_escape_macro(
+        :(default_buffer()),
+        :(@sneaky_return sum(alloc(Int, 10) .= 1)),
+        @__MODULE__()
+    )
+    @test_throws Exception Bumper.Internals._no_escape_macro(
+        :(default_buffer()),
+        :(@goto lab),
+        @__MODULE__()
+    )
+    @test_throws Exception Bumper.Internals._no_escape_macro(
+        :(default_buffer()),
+        :(@sneaky_goto lab),
+        @__MODULE__()
+    )
 end
 
 @testset "tasks and buffer switching" begin
