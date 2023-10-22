c964717d2fa3caf3f1c529d5a2d25de59ad58a4e
Mason Protter <mason.protter@icloud.com>
2023-10-22 23:51:54 +0200

    [Breaking] Replace `alloc` with `@alloc` (#20)
    * Replace `alloc` with safer `@alloc` macro
    
    * try using src for docstrings
    
    * fix docstring display (kinda)
    
    * remove `no_escape` functional, increase coverage
    
    * fixes
    
    * fix tests again
    
    * add checkpoint_save and checkpoint_restore! functions
    
    * experimental package -> package

--- src/Bumper.jl
+++ src/Bumper.jl
@@ -1,157 +1,171 @@
 module Bumper
 
-export AllocBuffer, alloc, alloc_nothrow, default_buffer, @no_escape, with_buffer
+export AllocBuffer, @alloc, default_buffer, @no_escape, with_buffer
 
 
-## Public API
-# ------------------------------------------------------
+
+"""
+    AllocBuffer{StorageType}
+
+This is a single bump allocator that could be used to store some memory of type `StorageType`.
+Do not manually manipulate the fields of an AllocBuffer that is in use.
+"""
 mutable struct AllocBuffer{Storage}
     buf::Storage
     offset::UInt
 end
 
-function default_buffer end
-function alloc end
+"""
+    @no_escape([buf=default_buffer()], expr)
+
+Record the current state of `buf` (which defaults to the `default_buffer()` if there is only one argument), and then run
+the code in `expr` and then reset `buf` back to the state it was in before the code ran. This allows us to allocate memory
+within the `expr` using `@alloc`, and then have those arrays be automatically de-allocated once the expression is over. This
+is a restrictive but highly efficient form of memory management.
+
+See also `Bumper.checkpoint_save`, and `Bumper.checkpoint_restore!`.
+
+Using `return`, `@goto`, and `@label` are not allowed inside of `@no_escape` block.
+
+Example:
+
+    function f(x::Vector{Int})
+        # Set up a scope where memory may be allocated, and does not escape:
+        @no_escape begin
+            # Allocate a `PtrArray` from StrideArraysCore.jl using memory from the default buffer.
+            y = @alloc(Int, length(x))
+            # Now do some stuff with that vector:
+            y .= x .+ 1
+           sum(y)
+        end
+    end
+"""
 macro no_escape end
-function no_escape end
-function with_buffer end
-function set_default_buffer_size! end
-allow_ptr_array_to_escape() = false
+
+"""
+    @alloc(T, n::Int...) -> PtrArray{T, length(n)}
+
+This can only be used inside a `@no_escape` block to allocate a `PtrArray` whose dimensions
+are determined by `n`. The memory used to allocate this array will come from the buffer
+associated with the enclosing `@no_escape` block.
+
+Do not allow any references to these arrays to escape the enclosing `@no_escape` block, and do
+not pass these arrays to concurrent tasks unless that task is guaranteed to terminate before the
+`@no_escape` block ends. Any array allocated in this way which is found outside of it's parent
+`@no_escape` block has undefined contents.
+"""
+macro alloc(args...)
+    error("The @alloc macro may only be used inside of a @no_escape block.")
+end
+
+"""
+    @alloc_nothrow(T, n::Int...) -> PtrArray{T, length(n)}
+
+Just like `@alloc` but it won't throw an error if the size you requested is too big. Don't use this
+unless you're doing something weird like using StaticCompiler.jl and can't handle errors.
+"""
+macro alloc_nothrow(args...)
+    error("The @alloc macro may only be used inside of a @no_escape block.")
+end
+
+"""
+    Bumper.alloc_nothrow(T, buf, n::Int...) -> PtrArray{T, length(n)}
+
+Just like `Bumper.alloc` but it won't throw an error if the size you requested is too big. Don't use this
+unless you're doing something weird like using StaticCompiler.jl and can't handle errors.
+"""
 function alloc_nothrow end
+
+"""
+    default_buffer() -> AllocBuffer{Vector{UInt8}}
+
+Return the current task-local default buffer, if one does not exist in the current task, it will
+create one.
+"""
+function default_buffer end
+
+"""
+    alloc(T, b::AllocBuffer, n::Int...) -> PtrArray{T, length(n)}
+
+Function-based alternative to `@alloc` which allocates onto a specified `AllocBuffer`.
+You must obey all the rules from `@alloc`, but you can use this outside of the lexical
+scope of `@no_escape` for specific (but dangerous!) circumstances where you cannot avoid
+a scope barrier between the two.
+"""
+function alloc end
+
+"""
+    with_buffer(f, buf::AllocBuffer{Vector{UInt8}})
+
+Execute the function `f()` in a context where `default_buffer()` will return `buf` instead of the normal `default_buffer`. This currently only works with `AllocBuffer{Vector{UInt8}}`.
+
+Example:
+
+    julia> let b1 = default_buffer()
+               b2 = AllocBuffer(10000)
+               with_buffer(b2) do
+                   @show default_buffer() == b2
+               end
+               @show default_buffer() == b1
+           end
+    default_buffer() == b2 = true
+    default_buffer() == b1 = true
+    true
+"""
+function with_buffer end
+
+"""
+    Bumper.set_default_buffer_size!(n::Int)
+
+Change the size (in number of bytes) of the default buffer. This should not be done
+while any buffers are in use, as their contents may become undefined.
+"""
+function set_default_buffer_size! end
+
+
+allow_ptr_array_to_escape() = false
+
+"""
+    Bumper.reset_buffer!(buf::AllocBuffer=default_buffer())
+
+This resets an AllocBuffer's offset to zero, effectively making it like a freshly allocated buffer. This might be
+necessary to use if you accidentally over-allocate a buffer.
+"""
 function reset_buffer! end
+
+
+struct Checkpoint{Store}
+    buf::AllocBuffer{Store}
+    offset::UInt
+end
+
+"""
+    Bumper.checkpoint_save(buf::AllocBuffer = default_buffer()) -> Checkpoint
+
+Returns a `Checkpoint` object which stores the state of an `AllocBuffer` at a given point in
+a program. One can then use `Bumper.checkpoint_restore!(cp::Checkpoint)` to later on restore
+the state of the buffer to it's earlier saved state, undoing any bump allocations which
+happened in the meantime on that buffer.
+
+Users should prefer to use `@no_escape` instead of `checkpoint_save` and `checkpoint_restore`,
+which is a safer and more structured way of doing the same thing.
+"""
+function checkpoint_save end
+
+"""
+    Bumper.checkpoint_restore!(cp::Checkpoint)
+
+Restore a buffer (the one used to create the checkpoint) to the state it was in when the
+checkpoint was created, undoing any bump allocations which happened in the meantime on that
+buffer. See also `Bumper.checkpoint_save`
+
+Users should prefer to use `@no_escape` instead of `checkpoint_save` and `checkpoint_restore`,
+which is a safer and more structured way of doing the same thing.
+"""
+function checkpoint_restore! end
 
 ## Private
 # ------------------------------------------------------
-module Internals
-
-using StrideArraysCore, MacroTools
-import Bumper: AllocBuffer,  alloc, default_buffer, allow_ptr_array_to_escape, set_default_buffer_size!,
-    with_buffer, no_escape, @no_escape, alloc_nothrow, reset_buffer!
-
-function total_physical_memory()
-    @static if isdefined(Sys, :total_physical_memory)
-        Sys.total_physical_memory()
-    elseif isdefined(Sys, :physical_memory)
-        Sys.physical_memory()
-    else
-        Sys.total_memory()
-    end
-end
-
-const default_buffer_key = gensym(:buffer)
-const buffer_size = Ref(total_physical_memory() ÷ 8)
-
-Base.pointer(b::AllocBuffer) = pointer(b.buf)
-
-AllocBuffer(max_size::Int)  = AllocBuffer(Vector{UInt8}(undef, max_size), UInt(0))
-AllocBuffer(storage) = AllocBuffer(storage, UInt(0))
-AllocBuffer() = AllocBuffer(Vector{UInt8}(undef, buffer_size[]), UInt(0))
-
-function default_buffer()
-    get!(() -> AllocBuffer(), task_local_storage(), default_buffer_key)::AllocBuffer{Vector{UInt8}}
-end
-
-function set_default_buffer_size!(sz::Int)
-    buffer_size[] = sz
-    resize!(default_buffer(), sz)
-    GC.gc()
-    sz
-end
-
-function reset_buffer!(b::AllocBuffer = default_buffer())
-    if b === default_buffer()
-        b.buf = Vector{UInt8}(undef, buffer_size[])
-        GC.gc()
-    end
-    b.offset = UInt(0)
-end
-
-function alloc_ptr(b::AllocBuffer, sz::Int)
-    ptr = pointer(b) + b.offset
-    b.offset += sz
-    b.offset > sizeof(b.buf) && oom_error(b)
-    ptr
-end
-
-function alloc_ptr_nothrow(b::AllocBuffer, sz::Int)
-    ptr = pointer(b) + b.offset
-    b.offset += sz
-    ptr
-end
-
-
-@noinline function oom_error(b)
-    error("alloc: Buffer out of memory. This might be a sign of a memory leak.
-Use Bumper.reset_buffer!() or Bumper.reset_buffer!(b::AllocBuffer) to reclaim its memory.")
-end
-
-function no_escape(f, b::AllocBuffer)
-    offset = b.offset
-    res = f()
-    b.offset = offset
-    if res isa PtrArray && !(allow_ptr_array_to_escape())
-        esc_err()
-    end
-    res
-end
-no_escape(f) = no_escape(f, default_buffer())
-
-macro no_escape(b_ex, ex)
-    _no_escape_macro(b_ex, ex, __module__)
-end
-
-macro no_escape(ex)
-    _no_escape_macro(:(default_buffer()), ex, __module__)
-end
-
-function _no_escape_macro(b_ex, ex, __module__)
-    @gensym b offset
-    e_offset = esc(offset)
-    e_b = esc(b)
-    # We need to macroexpand the code in case the user has a macro in the body which has return or goto in it
-    MacroTools.postwalk(macroexpand(__module__, ex)) do x
-        @gensym rv
-        if MacroTools.isexpr(x, :return) 
-            error("The `return` keyword is not allowed to be used inside the `@no_escape` macro")
-        elseif MacroTools.isexpr(x, :symbolicgoto) # && (x.args[1] ∉ enclosed_labels)
-            error("`@goto` statements are not allowed to be used inside the `@no_escape` macro")
-        end
-    end
-    quote
-        $e_b = $(esc(b_ex))
-        $e_offset = getfield($e_b, :offset)
-        res = $(esc(ex))
-        $e_b.offset = $e_offset
-        if res isa PtrArray && !(allow_ptr_array_to_escape())
-           esc_err()
-        end
-        res
-    end
-end
-
-@noinline esc_err() =
-    error("Tried to return a PtrArray from a `no_escape` block. If you really want to do this, evaluate Bumper.allow_ptrarray_to_escape() = true")
-
-with_buffer(f, b::AllocBuffer) = task_local_storage(f, default_buffer_key, b)
-
-struct NoThrow end
-
-function StrideArraysCore.PtrArray{T}(b::AllocBuffer, s::Vararg{Integer, N}) where {T, N}
-    ptr = convert(Ptr{T}, alloc_ptr(b, prod(s) * sizeof(T)))
-    PtrArray(ptr, s)
-end
-
-alloc(::Type{T}, s::Integer...) where {T} = PtrArray{T}(default_buffer(), s...)
-alloc(::Type{T}, buf::AllocBuffer, s::Integer...) where {T} = PtrArray{T}(buf, s...)
-
-function StrideArraysCore.PtrArray{T}(b::AllocBuffer, ::NoThrow, s::Vararg{Integer, N}) where {T, N}
-    ptr = convert(Ptr{T}, alloc_ptr_nothrow(b, prod(s) * sizeof(T)))
-    PtrArray(ptr, s)
-end
-
-alloc_nothrow(::Type{T}, buf::AllocBuffer, s...) where {T} = PtrArray{T}(buf, NoThrow(), s...) 
-
-
-end # Internals
+include("internals.jl")
 
 end # Bumper
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,38 +2,81 @@ using Test, Bumper
 
 # set_default_buffer_size!(1000)
 
-function f(x::Vector{Int})
-    @no_escape begin
-        y = alloc(Int, length(x))
+function f(x, buf=default_buffer())
+    @no_escape buf begin
+        y = @alloc(Int, length(x))
         y .= x .+ 1
         sum(y)
     end
 end
 
-function f(x, buf::AllocBuffer)
+function g(x, buf::AllocBuffer)
     @no_escape buf begin 
-        y = alloc(Int, buf, length(x)) 
+        y = Bumper.alloc(Int, buf, length(x)) 
         y .= x .+ 1
         sum(y)
     end
 end
+
+
 
 @testset "basic" begin
     v = [1,2,3]
     b = AllocBuffer(100)
 
     @test f(v) == 9
+    @test b.offset == 0
     @test f(v, b) == 9
+    @test b.offset == 0
+    @test g(v, b) == 9
+    @test b.offset == 0
     
     @test @allocated(f(v)) == 0
     @test @allocated(f(v, b)) == 0
+    @test @allocated(g(v, b)) == 0
 
-    @test b.offset == 0
-    
-    @test_throws Exception alloc(Int, b, 100000)
+    @no_escape b begin
+        y = @alloc(Int, length(v))
+        off1 = b.offset
+        @no_escape b begin
+            z = @alloc(Int, length(v))
+            
+            @test pointer(z) != pointer(y)
+            @test Int(pointer(z)) == Int(pointer(y)) + 8*length(v)
+            @test b.offset == off1 + 8*length(v)
+        end
+        b2 = AllocBuffer(100)
+        @no_escape b2 begin
+            z = @alloc(Int, length(v))
+            @test pointer(z) == pointer(b2) 
+        end
+        
+        @test b.offset == off1
+    end
+
+    let b1 = default_buffer()
+        b2 = AllocBuffer(Vector{UInt8}(undef, 100))
+        with_buffer(b2) do
+            @test default_buffer() == b2
+        end
+        @test default_buffer() == b1
+    end
+    let b2 = AllocBuffer(Vector{Int}(undef, 100))
+        @test_throws MethodError with_buffer(b2) do
+            default_buffer()
+        end 
+    end
+
+    @test_throws Exception Bumper.alloc(Int, b, 100000)
     Bumper.reset_buffer!(b)
+    Bumper.reset_buffer!()
     @test_throws Exception @no_escape begin
         alloc(Int, 10)
+    end
+
+    @no_escape b begin
+        v = @alloc_nothrow(Int, 100000)
+        @test 8*length(v) > length(b.buf)
     end
 end
 
@@ -72,6 +115,11 @@ end
     @test_throws Exception Bumper.Internals._no_escape_macro(
         :(default_buffer()),
         :(@sneaky_goto lab),
+        @__MODULE__()
+    )
+    @test_throws Exception Bumper.Internals._no_escape_macro(
+        :(default_buffer()),
+        :(@label lab),
         @__MODULE__()
     )
 end
