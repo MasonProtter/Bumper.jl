0016e54788ecc5fe63fe0f8e3f9c4c37935e0ea9
Mason Protter <mason.protter@icloud.com>
2023-11-14 23:13:15 +0100

    fix docstrings

--- src/Bumper.jl
+++ src/Bumper.jl
@@ -38,7 +38,7 @@ This can be used inside a `@no_escape` block to allocate a `PtrArray` whose dime
 are determined by `n`. The memory used to allocate this array will come from the buffer
 associated with the enclosing `@no_escape` block.
 
-Do not allow any references to these arrays to escape the enclosing `@no_escape` block, and do
+Do not allow any references to this array to escape the enclosing `@no_escape` block, and do
 not pass these arrays to concurrent tasks unless that task is guaranteed to terminate before the
 `@no_escape` block ends. Any array allocated in this way which is found outside of its parent
 `@no_escape` block has undefined contents, and writing to this pointer will have undefined behaviour.
@@ -50,11 +50,11 @@ end
 """
     @alloc_ptr(n::Integer) -> Ptr{Nothing}
 
-This can be used inside a `@no_escape` block to allocate a pointer whose dimensions
-are determined by `n`. The memory used to allocate this array will come from the buffer
-associated with the enclosing `@no_escape` block.
+This can be used inside a `@no_escape` block to allocate a pointer which can hold `n` bytes.
+The memory used to allocate this pointer will come from the buffer associated with the
+enclosing `@no_escape` block.
 
-Do not allow any references to these pointers to escape the enclosing `@no_escape` block, and do
+Do not allow any references to this pointer to escape the enclosing `@no_escape` block, and do
 not pass these pointers to concurrent tasks unless that task is guaranteed to terminate before the
 `@no_escape` block ends. Any pointer allocated in this way which is found outside of its parent
 `@no_escape` block has undefined contents, and writing to this pointer will have undefined behaviour.
